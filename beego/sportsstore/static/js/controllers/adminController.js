af8aeb13cb9cd1e5dd42f3ec14f15bf82eff10d2
masihua123 <890721msh>
2017-02-17 17:40:36 -0600

    Beego+AnugularJS

--- beego/sportsstore/static/js/controllers/adminController.js
+++ beego/sportsstore/static/js/controllers/adminController.js
@@ -3,6 +3,7 @@
  */
 angular.module("sportsStoreAdmin")
     .constant("authUrl", "http://localhost:2403/users/login")
+    .constant("ordersUrl", "http://localhost:2403/orders")
     .controller("authCtrl", function($scope, $http, $location, authUrl) {
 
         $scope.authenticate = function (user, pass) {
@@ -11,12 +12,10 @@ angular.module("sportsStoreAdmin")
                 password: pass
             }, {
                 withCredentials: true
-            }).success(function (data) {
-                $scope.goPath = function(){
-                    $location.path('/main');
-                }
-            }).error(function (error) {
-                $scope.authenticationError = error;
+            }).then(function successCallback (response) {
+                $location.path("/main");
+            },function errorCallback(response){
+                $scope.authenticationError = response.status;
             });
         }
     })
@@ -33,4 +32,27 @@ angular.module("sportsStoreAdmin")
             return $scope.current == "Products"
                 ? "/static/js/views/adminProducts.html" : "/static/js/views/adminOrders.html";
         };
+    })
+    .controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+
+        $http.get(ordersUrl, { withCredentials: true })
+            .then(function successCallback (response) {
+                $scope.orders = response.data;
+            },function  errorCallback(response) {
+                $scope.error=response.status;
+            });
+        $scope.selectedOrder;
+
+        $scope.selectOrder = function (order) {
+            $scope.selectedOrder = order;
+        };
+
+        $scope.calcTotal = function (order) {
+            var total = 0;
+            for (var i = 0; i < order.products.length; i++) {
+                total +=
+                    order.products[i].count * order.products[i].price;
+            }
+            return total;
+        }
     });
